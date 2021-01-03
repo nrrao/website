451da31368b70c84d54b8e2b36816cab42968240
nrrao <nandanarao@yahoo.com>
2021-01-03 17:48:41 -0500

    view counter incrementing

--- src/components/FAQCard/faqHook.js
+++ src/components/FAQCard/faqHook.js
@@ -2,10 +2,10 @@ import React, { useState,useEffect } from 'react';
 import { ContactSupportOutlined } from '@material-ui/icons';
 
 const useFaqs = () => {
-  // 1
-  const [faqs, setFaqs] = React.useState([]);
+ 
+  const [faqs, setFaqs] = useState([]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     async function fetchFaqs() {
       const fullResponse = await fetch('http://test-civictechindexadmin.herokuapp.com/api/faqs/');
       const responseJson = await fullResponse.json();
@@ -16,33 +16,6 @@ const useFaqs = () => {
     fetchFaqs();
   }, []);
 
-  // useEffect(() => {
-  //   //make API calls
-  //   fetch(
-  //     `http://test-civictechindexadmin.herokuapp.com/api/faqs/`,
-  //     {
-  //       method: "GET",
-      
-  //     }
-  //   )
-  //     .then((response) => {
-  //       if (response.ok) {
-  //         console.log(response);
-  //         return response.json();
-  //       } else {
-  //         throw new Error("Something went wrong");
-  //       }
-  //     })
-  //     .then((data) => {
-  //       console.log(data);
-  //       setFaqs(data);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // },[]);
-
-  // 2
   return [faqs];
 };
 
--- src/components/FAQCard/index.js
+++ src/components/FAQCard/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { createRef, useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import useStyles from './styles.js';
 import Accordion from "@material-ui/core/Accordion";
@@ -8,27 +8,58 @@ import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import {Grid, Divider } from "@material-ui/core";
 
-import useFaqs from "./faqHook";
+//import useFaqs from "./faqHook";
 
 
 
 export default function FAQCard(props) {
-  //const faqs = props.faqs
+
   const classes = useStyles();
-  const [faqs] = useFaqs();
-  console.log(faqs);
-  const [openFAQCard, setOpenFAQCard] = useState(true);
+  //const [faqs] = useFaqs();
+ // console.log(faqs);
+  const [faqs, setFaqs] = useState([]);
+  const [currentFaq, setCurrentFaq] = useState([]);
   const [sendRequest, setSendRequest] = useState(false);
 
   useEffect(() => {
-    if(sendRequest){
-       //send the request
-       setSendRequest(false);
+    
+    async function fetchFaqs() {
+      const fullResponse = await fetch('http://test-civictechindexadmin.herokuapp.com/api/faqs/');
+      const responseJson = await fullResponse.json();
+      console.log(responseJson)
+      setFaqs(responseJson);
     }
+
+    fetchFaqs();
+    
+    console.log(currentFaq)
+    console.log(sendRequest)
+    async function incrementViewCount(){
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ "question":currentFaq.question,
+        "answer": currentFaq.answer,
+        "view_count": currentFaq.view_count })
+      }
+      console.log(requestOptions)
+      const response = await fetch(`http://test-civictechindexadmin.herokuapp.com/api/faqs/${currentFaq.id}/increment_count/`, requestOptions);
+      
+    }
+    if(sendRequest){
+        console.log('++++++++')
+       //send the request
+      incrementViewCount()
+      setSendRequest(false);
+    }
+   
   },
-  [sendRequest]);
+  [sendRequest,currentFaq]);
+
+
 
   return (
+    <>
     <div style={{backgroundColor:'#F2F2F2'}}>
     <Grid container  alignItems="center" justify="center" style={{padding:'60px'}} >
       <Grid item xs={12} lg={7} style={{paddingBottom:'25px'}}>
@@ -39,27 +70,27 @@ export default function FAQCard(props) {
     <Grid item xs={12} lg={7} >
         {faqs.map((faq) => {
         return (
-          <>
+          <div key={faq.id}>
             <Accordion style={{ marginBottom: "20px" }}>
               <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
-                id={faq.key}
-                disabled={sendRequest} onClick={() => setSendRequest(true)}
+                disabled={sendRequest} onClick={() => {setSendRequest(true);setCurrentFaq(faq)}}
               >
-                <h6 className={classes.questionText}>
+                <h6 style={{padding:'0 25px',margin:'0', fontSize:'20px'}}>
                   {faq.question}
                 </h6>
               </AccordionSummary>
               <Divider />
               <AccordionDetails>
-                <Typography className={classes.answerText}>{faq.answer}</Typography>
+                <Typography style={{padding:'0 25px'}}>{faq.answer}</Typography>
               </AccordionDetails>
             </Accordion>
-          </>
+          </div>
         );
       })}
     </Grid>
     </Grid>
     </div>
+    </>
   );
 }
